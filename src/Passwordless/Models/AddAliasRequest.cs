76d82f5c0306cf5af4f2155d704a4fd90d30f59d
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-09-21 14:21:33 +0200

    fix build

--- src/Passwordless/Models/AddAliasRequest.cs
+++ src/Passwordless/Models/AddAliasRequest.cs
@@ -2,31 +2,28 @@ namespace Passwordless.Models;
 
 public class AddAliasRequest
 {
-    public AddAliasRequest(string userId, string alias, bool hashing = true)
-        : this(userId, hashing)
+    public AddAliasRequest(string userId, bool hashing = true)
+        : this(userId, new HashSet<string>(), hashing)
     {
-        Aliases = new();
-        if (!string.IsNullOrWhiteSpace(alias))
-        {
-            Aliases = new() { alias };
-        }
+    }
+
+    public AddAliasRequest(string userId, string alias, bool hashing = true)
+        : this(userId, new HashSet<string> { alias }, hashing)
+    {
     }
 
     public AddAliasRequest(string userId, HashSet<string> aliases, bool hashing = true)
-        : this(userId, hashing)
-    {
-        if (aliases == null) return;
-
-        Aliases = new HashSet<string>(aliases.Where(x => !string.IsNullOrWhiteSpace(x)));
-    }
-
-    private AddAliasRequest(string userId, bool hashing = true)
     {
         UserId = userId ?? throw new ArgumentNullException(nameof(userId));
         Hashing = hashing;
+
+        Aliases = aliases == null
+            ? new HashSet<string>()
+            : new HashSet<string>(aliases.Where(x => !string.IsNullOrWhiteSpace(x)));
     }
 
     public string UserId { get; }
+
     public HashSet<string> Aliases { get; }
 
     /// <summary>
