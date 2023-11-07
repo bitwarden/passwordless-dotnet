a2b8ed731f681b8f633c363e8220ecef0c10201c
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-11-07 14:25:31 +0200

    asd

--- src/Passwordless/PasswordlessClient.cs
+++ src/Passwordless/PasswordlessClient.cs
@@ -11,6 +11,11 @@ namespace Passwordless;
 [DebuggerDisplay("{DebuggerToString(),nq}")]
 public class PasswordlessClient : IPasswordlessClient, IDisposable
 {
+    private static readonly string SdkVersion =
+        typeof(PasswordlessClient).Assembly.GetName().Version?.ToString(3) ??
+        // This should never happen, unless the assembly had its metadata trimmed
+        throw new InvalidOperationException("Could not determine SDK version.");
+
     private readonly HttpClient _http;
     private readonly PasswordlessOptions _options;
 
@@ -27,7 +32,7 @@ public class PasswordlessClient : IPasswordlessClient, IDisposable
                 },
                 {
                     "Client-Version",
-                    $".NET-{typeof(PasswordlessClient).Assembly.GetName().Version.ToString(3)}"
+                    $".NET-{SdkVersion}"
                 }
             }
         };
