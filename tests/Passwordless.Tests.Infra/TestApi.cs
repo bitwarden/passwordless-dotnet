c9c203ee7d7be03faebc08a63815923b56fd6b3d
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-11-30 16:32:51 +0200

    asd

--- tests/Passwordless.Tests.Infra/TestApi.cs
+++ tests/Passwordless.Tests.Infra/TestApi.cs
@@ -33,7 +33,7 @@ public class TestApi : IAsyncDisposable
         _apiContainer = new ContainerBuilder()
             // https://github.com/passwordless/passwordless-server/pkgs/container/passwordless-test-api
             .WithImage("ghcr.io/passwordless/passwordless-test-api:latest") // switch to stable eventually
-            // Make sure we always have the latest version of the image
+                                                                            // Make sure we always have the latest version of the image
             .WithImagePullPolicy(PullPolicy.Always)
             // Run in development environment to enable migrations
             .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
