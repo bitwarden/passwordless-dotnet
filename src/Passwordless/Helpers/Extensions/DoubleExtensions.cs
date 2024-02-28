a19443f104c70e42705176b75c0fb8d89d23d8c8
jrmccannon <jmccannon@bitwarden.com>
2024-02-28 10:22:48 -0600

    Added internal ToInt extension to double. Updated email used in tests.

---
+++ src/Passwordless/Helpers/Extensions/DoubleExtensions.cs
@@ -0,0 +1,6 @@
+namespace Passwordless.Helpers.Extensions;
+
+internal static class DoubleExtensions
+{
+    internal static int ToInt(this double value) => (int)value;
+}
--- tests/Passwordless.Tests/MagicLinksTests.cs
+++ tests/Passwordless.Tests/MagicLinksTests.cs
@@ -12,11 +12,25 @@ namespace Passwordless.Tests;
 public class MagicLinksTests(TestApiFixture api, ITestOutputHelper testOutput) : ApiTestBase(api, testOutput)
 {
     [Fact]
-    public async Task I_can_send_a_magic_link()
+    public async Task I_can_send_a_magic_link_with_a_specified_time_to_live()
     {
         // Arrange
         var passwordless = await Api.CreateClientAsync();
-        var request = new SendMagicLinkRequest("passwordless@dev.test", "https://www.example.com?token=$TOKEN", "user", new TimeSpan(0, 15, 0));
+        var request = new SendMagicLinkRequest("test@passwordless.dev", "https://www.example.com?token=$TOKEN", "user", new TimeSpan(0, 15, 0));
+
+        // Act
+        var action = async () => await passwordless.SendMagicLinkAsync(request, CancellationToken.None);
+
+        // Assert
+        await action.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public async Task I_can_send_a_magic_link_without_a_time_to_live()
+    {
+        // Arrange
+        var passwordless = await Api.CreateClientAsync();
+        var request = new SendMagicLinkRequest("test@passwordless.dev", "https://www.example.com?token=$TOKEN", "user", null);
 
         // Act
         var action = async () => await passwordless.SendMagicLinkAsync(request, CancellationToken.None);
