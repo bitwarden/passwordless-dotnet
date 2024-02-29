016ebbdf5ac27e1193b01112dd1a0157397172b0
jrmccannon <jmccannon@bitwarden.com>
2024-02-29 08:15:05 -0600

    Using convert instead of casting.

--- src/Passwordless/Helpers/Extensions/DoubleExtensions.cs
+++ src/Passwordless/Helpers/Extensions/DoubleExtensions.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Passwordless.Helpers.Extensions;
 
 internal static class DoubleExtensions
 {
-    internal static int ToInt(this double value) => (int)value;
+    internal static int ToInt(this double value) => Convert.ToInt32(value);
 }
