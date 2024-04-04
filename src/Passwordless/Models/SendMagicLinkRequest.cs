79b4b6ee623b5fd5e470785bef9f065009a3c817
Jared McCannon <jmccannon@bitwarden.com>
2024-04-04 10:55:48 -0500

    Adding Magic Links (#116)
    Added Magic links to the SDK
    
    Co-authored-by: Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
    Co-authored-by: Jonas Hendrickx <jhendrickx@bitwarden.com>

---
+++ src/Passwordless/Models/SendMagicLinkRequest.cs
@@ -0,0 +1,23 @@
+using System;
+using Passwordless.Helpers.Extensions;
+
+namespace Passwordless.Models;
+
+/// <summary>
+/// Request for sending an email with a link that contains a 1-time-use token to be used for validating signin.
+/// </summary>
+/// <param name="EmailAddress">Valid email address that will be the recipient of the magic link email</param>
+/// <param name="UrlTemplate">Url template that needs to contain the token template, <token>. The token template will be replaced with a valid signin token to be sent to the verify sign in token endpoint (https://v4.passwsordless.dev/signin/verify).</param>
+/// <param name="UserId">Identifier for the user the email is intended for.</param>
+/// <param name="TimeToLive">Length of time the magic link will be active. Default value will be 15 minutes.</param>
+public record SendMagicLinkRequest(string EmailAddress, string UrlTemplate, string UserId, TimeSpan? TimeToLive)
+{
+    internal SendMagicLinkApiRequest ToRequest() =>
+        new(
+            this.EmailAddress,
+            this.UrlTemplate,
+            this.UserId,
+            this.TimeToLive?.TotalSeconds.Pipe(Convert.ToInt32));
+};
+
+internal record SendMagicLinkApiRequest(string EmailAddress, string UrlTemplate, string UserId, int? TimeToLive);
