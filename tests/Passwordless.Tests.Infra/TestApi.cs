b34f619f7056fa9d05c24db5ce35cbe1118dca8a
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-05-22 19:39:42 +0300

    asd

--- tests/Passwordless.Tests.Infra/TestApi.cs
+++ tests/Passwordless.Tests.Infra/TestApi.cs
@@ -40,6 +40,8 @@ public class TestApi : IAsyncDisposable
             .WithEnvironment("ASPNETCORE_HTTP_PORTS", ApiPort.ToString(CultureInfo.InvariantCulture))
             // We need the management key to create apps
             .WithEnvironment("PasswordlessManagement__ManagementKey", ManagementKey)
+            // Set the mail sink file path to a known value
+            .WithEnvironment("Mail__File__Path", "mail.md")
             .WithPortBinding(ApiPort, true)
             // Wait until the API is launched, has performed migrations, and is ready to accept requests
             .WithWaitStrategy(Wait
