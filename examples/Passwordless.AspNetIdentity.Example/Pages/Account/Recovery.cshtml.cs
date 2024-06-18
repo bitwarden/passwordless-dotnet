53d8f5ea4d0e29364e08d41b0a943397cc462c99
jrmccannon <jmccannon@bitwarden.com>
2024-06-18 11:56:18 -0500

    Added new testing ability for manually generated tokens and magic links. Showing claims on hello world to show which claims the user has.

--- examples/Passwordless.AspNetIdentity.Example/Pages/Account/Recovery.cshtml.cs
+++ examples/Passwordless.AspNetIdentity.Example/Pages/Account/Recovery.cshtml.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Specialized;
 using System.ComponentModel.DataAnnotations;
-using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
@@ -22,6 +20,9 @@ public class Recovery : PageModel
     private readonly PasswordlessClient _passwordlessClient;
     private readonly IUrlHelperFactory _urlHelperFactory;
     private readonly IActionContextAccessor _actionContextAccessor;
+
+    public const string MagicLink = "magic_link";
+    public const string GeneratedSignIn = "generated_signin";
 
     public RecoveryForm Form { get; } = new();
 
@@ -61,18 +62,41 @@ public class Recovery : PageModel
             throw new InvalidOperationException("ActionContext is null");
         }
 
-        var token = await _passwordlessClient.GenerateAuthenticationTokenAsync(new AuthenticationOptions(user.Id), cancellationToken);
         var urlBuilder = _urlHelperFactory.GetUrlHelper(_actionContextAccessor.ActionContext);
         var url = urlBuilder.PageLink("/Account/Magic") ?? urlBuilder.Content("~/");
 
         var uriBuilder = new UriBuilder(url);
         var query = HttpUtility.ParseQueryString(uriBuilder.Query);
-        query["token"] = "";
+        query["token"] = string.Empty;
         uriBuilder.Query = query.ToString();
+        var successMessage = string.Empty;
 
-        await _passwordlessClient.SendMagicLinkAsync(new SendMagicLinkRequest(form.Email!, uriBuilder.Uri + "$TOKEN", user.Id, null), cancellationToken);
+        if (string.Equals(form.RecoveryMethod, MagicLink, StringComparison.OrdinalIgnoreCase))
+        {
+            await _passwordlessClient.SendMagicLinkAsync(new SendMagicLinkRequest(form.Email!, uriBuilder.Uri + "$TOKEN", user.Id, null), cancellationToken);
 
-        return RedirectToPage("Recovery", "SuccessfulRecovery", new { message = "message" });
+            successMessage = "Check your API magic link destination (mail.md if running local, email if using cloud.";
+        }
+        else if (string.Equals(form.RecoveryMethod, GeneratedSignIn, StringComparison.OrdinalIgnoreCase))
+        {
+            var token = await _passwordlessClient.GenerateAuthenticationTokenAsync(new AuthenticationOptions(user.Id), cancellationToken);
+            query["token"] = token.Token;
+            uriBuilder.Query = query.ToString();
+
+            var message = $"""
+                           New message:
+
+                           This was generated with manually generated authentication token.
+                           <a href="{uriBuilder}">Link<a>
+                           {Environment.NewLine}
+                           """;
+
+            await System.IO.File.AppendAllTextAsync("mail.md", message, cancellationToken);
+
+            successMessage = message;
+        }
+
+        return RedirectToPage("Recovery", "SuccessfulRecovery", new { message = successMessage });
     }
 }
 
@@ -81,4 +105,7 @@ public class RecoveryForm
     [EmailAddress]
     [Required]
     public string? Email { get; set; }
+
+    [Required]
+    public string RecoveryMethod { get; set; } = string.Empty;
 }
--- examples/Passwordless.AspNetIdentity.Example/Pages/Authorized/HelloWorld.cshtml.cs
+++ examples/Passwordless.AspNetIdentity.Example/Pages/Authorized/HelloWorld.cshtml.cs
@@ -1,16 +1,24 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Passwordless.AspNetIdentity.Example.Pages.Authorized;
 
-public class HelloWorldModel(ILogger<HelloWorldModel> logger) : PageModel
+public class HelloWorldModel(ILogger<HelloWorldModel> logger, UserManager<IdentityUser> userManager) : PageModel
 {
-    public void OnGet()
+    public async Task OnGetAsync()
     {
         var identity = HttpContext.User.Identity!;
         var email = User.FindFirstValue(ClaimTypes.Email)!;
         AuthenticatedUser = new AuthenticatedUserModel(identity.Name!, email);
+        Claims = (await userManager.GetClaimsAsync((await userManager.GetUserAsync(User))!))
+            .Select(ClaimRecord.GetInstance)
+            .Concat(User.Claims.Select(ClaimRecord.GetInstance));
     }
 
     public void OnPost(string? nickname)
@@ -26,6 +34,13 @@ public class HelloWorldModel(ILogger<HelloWorldModel> logger) : PageModel
     public string? Nickname { get; set; }
 
     public bool CanAddPassKeys { get; set; }
+
+    public IEnumerable<ClaimRecord> Claims { get; set; } = ArraySegment<ClaimRecord>.Empty;
 }
 
 public record AuthenticatedUserModel(string Username, string Email);
+
+public record ClaimRecord(string ClaimName, string ClaimValue)
+{
+    public static ClaimRecord GetInstance(Claim claim) => new(claim.Type, claim.Value);
+};
